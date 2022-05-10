38d7aafac79a8995f70452d1dc937124c2293652
Aleksandr Shatskikh <work@errand.ru>
2022-05-10 21:58:49 +0300

    feat: done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Form from './components/Form';
+import ProductsList from './components/ProductsList';
+import Filter from "./components/Filter";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="d-flex justify-content-between align-items-end mb-3">
+        <h1 className="title">Приглядись к этим предложениям</h1>
+        <Filter />
+      </div>
+      <Form />
+      <ProductsList />
     </div>
   );
-}
+};
 
-export default App;
+export default App
